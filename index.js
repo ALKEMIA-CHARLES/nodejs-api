593fb5c1274242531642e951ec577ed477a650c5
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2021-06-02 12:18:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')();
+const app = express();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+
+app.use(express.json())
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'x',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'we need a logo!' })
+    }
+
+    res.send({
+        tshirt: `x with your ${logo} and ID of ${id}`,
+    });
+});
